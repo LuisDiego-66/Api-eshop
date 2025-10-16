25ca0c6536b844166fbcb1410dfa466c0dff25bd
Diego Borja <luisdiegoborja8@gmail.com>
2025-10-16 14:05:12 -0400

    feat: mejorar mesaje de manejo de errores generales

--- src/common/helpers/handleDBExceptions.ts
+++ src/common/helpers/handleDBExceptions.ts
@@ -7,5 +7,5 @@ export function handleDBExceptions(error: any) {
   if (error.code === '23505') throw new ConflictException(error.detail); //! email
   if (error.code === '23503') throw new ConflictException(error.detail); //! key not exist
 
-  throw new InternalServerErrorException(error.message);
+  throw new InternalServerErrorException(error.detail);
 }
