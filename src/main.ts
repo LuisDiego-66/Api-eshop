82ceb953d7afbacd970faf2a5e11fc1d46f72db6
Diego Borja <luisdiegoborja8@gmail.com>
2025-11-15 09:36:49 -0400

    fix: agregar compatibilidad para crypto en entornos sin soporte nativo

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/environments/environments';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { setupSwagger } from './config/swagger/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const logger = new Logger('E-Shop');
+
+  //! errores de version de node en dockploy
+  if (!globalThis.crypto) {
+    // @ts-ignore
+    globalThis.crypto = { randomUUID };
+  }
 
   //! cors global enable
   app.enableCors();
