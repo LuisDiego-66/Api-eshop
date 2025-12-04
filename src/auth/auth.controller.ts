1e2deebfaf18c0eef2028970222e6243929b8cf1
Diego Borja <luisdiegoborja8@gmail.com>
2025-12-03 21:48:08 -0400

    refactor: descomentar endpoint de intercambio de Google y comentar callback

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -43,10 +43,10 @@ export class AuthController {
   //? ---------------------------------------------------------------------------------------------- */
 
   //se ejecuta cuando google me llama con sus datos directamente a mi servidor
-  @Get('google/callback')
+  //@Get('google/callback')
 
   //! se ejecuta cuando el front me llama con los datos de google
-  //@Post('google/exchange')
+  @Post('google/exchange')
   @UseGuards(GoogleOauthGuard)
   async googleAuthCallback(@Req() req /*  @Res() res: Response */) {
     const customer = req.user; //!  (en passport siempre en req.user)
